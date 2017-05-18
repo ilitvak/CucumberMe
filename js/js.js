914b235a5b7029273d8c33111d347e46ba7a128b
Irwin <irwin.litvak@gmail.com>
2017-05-18 16:07:06 -0400

    Came to a decision on the fonts for the site and uploaded iphone image

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    // Animates logo on window load.
+    $("#logo").css("transform", "scale(1)");
+    
+    
+    // shakes cucumber every 2 sec.
+    setTimeout(function(){
+          
+    }, 2000);
+});
+    console.log("wow");
+    
