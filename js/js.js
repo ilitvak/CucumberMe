17032087e406b464fbc5d3f18b518408073ce778
Irwin <irwin.litvak@gmail.com>
2017-05-25 14:30:55 -0400

    finished footer section animation and logic via js and starting on form in footer

--- js/js.js
+++ js/js.js
@@ -11,7 +11,23 @@ $(document).ready(function(){
         }
     });
     
+    // calculates top from footer section to faq section 
+
+    // Places footer section on top of faq section
+    $("footer").css("top", 
+        $("section.faq").offset().top - $("footer").offset().top
+    );
+    
     // pushes down footer section to reveal FAQ section
+    $(window).scroll(function(){
+        if( $(window).scrollTop() + $(window).height() >= $("section.faq").offset().top + 150  ) {
+            $("footer").css( "top", 0 );
+        }
+    })
+    
+    // Dynamic footer section height adjusted by section faq height
+    
+    $("footer").css( "height", $("section.faq").outerHeight() );
 
 });
     
